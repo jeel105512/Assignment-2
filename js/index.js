0f3d4362c1c0db2e3434e82ecaaf64ddcc40769a
JeelKumar Patel <111283792+jeel105512@users.noreply.github.com>
2022-11-10 13:30:51 -0800

    First Commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// Toggle theme
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click',() => {
+    let element = document.body;
+    element.classList.toggle('light-mode');
+})
